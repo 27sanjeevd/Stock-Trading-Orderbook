65f910580eefc43423eba445aa9d8a9de2b17658
Sanjeev Devarajan <realsanjeevd@gmail.com>
2023-02-01 10:27:01 -0500

    y

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 struct Order {
-    Order(double p, int v, int i, std::string n) : price(p), volume(v), id(i), name(n) {}
+    Order(double p, int v, int i, string& n) : price(p), volume(v), id(i), name(n) {}
     double price;
     int volume;
     int id;
@@ -26,23 +26,56 @@ class OrderBook {
         priority_queue<Order, vector<Order>, compare> sells;
 
     public:
-        Order *order = nullptr;
 
         void AddOrder(double price, int volume, int id, string name) {
-            order = new Order(price, volume, id, name);
-            priority_queue<Order, vector<Order>, compare> &order1 = price > 0 ? buys : sells;
-            order1.push({price, volume, id, name});
+            priority_queue<Order, vector<Order>, compare> &order = price > 0 ? buys : sells;
+            order.push({price, volume, id, name});
 
-            orders_.insert({price, *order});
-            delete order;
-            order = nullptr;
+        }
+        //make it so the function has some way of determining if an id corresponds to a buy or a sell (first few nums for example), to minimize costs
+        void RemoveOrder(double price, int id) {
+            priority_queue<Order, vector<Order>, compare> &order = price > 0 ? buys : sells;
+            priority_queue<Order, vector<Order>, compare> tempQueue;
+            while (!order.empty()) {
+                Order temp1 = order.top();
+                if (temp1.id == id) {
+                    order.pop();
+                    break;
+                }
+                tempQueue.push(temp1);
+                order.pop();
+            }
+
+            while (!tempQueue.empty()){
+                Order tempOrder = tempQueue.top();
+                order.push(tempOrder);
+                tempQueue.pop();
+            }
         }
 
-        void RemoveOrder(double price) {
-            auto it = orders_.find(price);
-            if (it != orders_.end()) {
-                orders_.erase(it);
+        void UpdateOrder(double price, int volume, int id){
+            priority_queue<Order, vector<Order>, compare> &order = price > 0 ? buys : sells;
+            priority_queue<Order, vector<Order>, compare> tempQueue;
+            while (!order.empty()) {
+                Order temp1 = order.top();
+                if (temp1.id == id) {
+                    tempQueue.push({temp1.price, volume, temp1.id, temp1.name});
+                    order.pop();
+                    break;
+                }
+                tempQueue.push(temp1);
+                order.pop();
             }
+
+            while (!tempQueue.empty()){
+                Order tempOrder = tempQueue.top();
+                order.push(tempOrder);
+                tempQueue.pop();
+            }
+        }
+
+        void Matching(){
+
         }
 
         void PrintOrders() {
@@ -67,9 +100,15 @@ int main() {
     book.AddOrder(100.0, 10, 1, "John");
     book.AddOrder(105.0, 20, 2, "Jane");
     book.AddOrder(102.0, 5, 3, "Jim");
-    book.AddOrder(93.0, 5, 3, "Henry");
-    book.AddOrder(115.2, 5, 3, "Austin");
-    book.AddOrder(13.0, 5, 3, "Manny");
+    book.AddOrder(93.0, 5, 4, "Henry");
+    book.AddOrder(115.2, 5, 5, "Austin");
+    book.AddOrder(13.0, 5, 6, "Manny");
+    book.AddOrder(-115.2, 5, 7, "Edward");
+    book.AddOrder(-13.0, 5, 9, "Arthur");
+    book.RemoveOrder(93.0, 4);
+    book.UpdateOrder(13.0, 10, 6);
     book.PrintOrders();
+
+    book.Matching();
     return 0;
 }
