1eca6f73d3938202393df764a8aa7b3a9b861536
Sanjeev Devarajan <realsanjeevd@gmail.com>
2023-02-11 22:22:23 -0500

    start

--- main.cpp
+++ main.cpp
@@ -5,110 +5,24 @@
 
 using namespace std;
 
-struct Order {
-    Order(double p, int v, int i, string& n) : price(p), volume(v), id(i), name(n) {}
-    double price;
-    int volume;
-    int id;
-    std::string name;
+enum OrderType {
+    BUY = 0,
+    SELL = 0,
 };
 
-struct compare {
-    bool operator()(const Order &a, const Order &b){
-        return a.price < b.price;
+struct Order {
+    double amount;
+    OrderType type;
+    double price;
+    int id;
+};
+
+struct OrderCompare {
+    bool operator()(const Order &a, const Order &b) const {
+        if (a.type == BUY) {
+            return a.price > b.price;
+        } else {
+            return a.price < b.price;
+        }
     }
 };
-
-class OrderBook {
-    private:
-        map<double, Order> orders_;
-        priority_queue<Order, vector<Order>, compare> buys;
-        priority_queue<Order, vector<Order>, compare> sells;
-
-    public:
-
-        void AddOrder(double price, int volume, int id, string name) {
-            priority_queue<Order, vector<Order>, compare> &order = price > 0 ? buys : sells;
-            order.push({price, volume, id, name});
-
-        }
-        //make it so the function has some way of determining if an id corresponds to a buy or a sell (first few nums for example), to minimize costs
-        void RemoveOrder(double price, int id) {
-            priority_queue<Order, vector<Order>, compare> &order = price > 0 ? buys : sells;
-            priority_queue<Order, vector<Order>, compare> tempQueue;
-            while (!order.empty()) {
-                Order temp1 = order.top();
-                if (temp1.id == id) {
-                    order.pop();
-                    break;
-                }
-                tempQueue.push(temp1);
-                order.pop();
-            }
-
-            while (!tempQueue.empty()){
-                Order tempOrder = tempQueue.top();
-                order.push(tempOrder);
-                tempQueue.pop();
-            }
-        }
-
-        void UpdateOrder(double price, int volume, int id){
-            priority_queue<Order, vector<Order>, compare> &order = price > 0 ? buys : sells;
-            priority_queue<Order, vector<Order>, compare> tempQueue;
-            while (!order.empty()) {
-                Order temp1 = order.top();
-                if (temp1.id == id) {
-                    tempQueue.push({temp1.price, volume, temp1.id, temp1.name});
-                    order.pop();
-                    break;
-                }
-                tempQueue.push(temp1);
-                order.pop();
-            }
-
-            while (!tempQueue.empty()){
-                Order tempOrder = tempQueue.top();
-                order.push(tempOrder);
-                tempQueue.pop();
-            }
-        }
-
-        void Matching(){
-
-        }
-
-        void PrintOrders() {
-            priority_queue<Order, vector<Order>, compare> copy = buys;
-
-            while (!copy.empty()) {
-                cout << copy.top().price << "\t" << copy.top().volume << "\t" << copy.top().id << "\t" << copy.top().name << endl;
-                copy.pop();
-            }
-            cout << "asdasd" << endl;
-            copy = sells;
-
-            while (!copy.empty()) {
-                cout << copy.top().price << "\t" << copy.top().volume << "\t" << copy.top().id << "\t" << copy.top().name << endl;
-                copy.pop();
-            }
-        }
-};
-
-int main() {
-    OrderBook book;
-    book.AddOrder(100.0, 10, 1, "John");
-    book.AddOrder(105.0, 20, 2, "Jane");
-    book.AddOrder(102.0, 5, 3, "Jim");
-    book.AddOrder(93.0, 5, 4, "Henry");
-    book.AddOrder(115.2, 5, 5, "Austin");
-    book.AddOrder(13.0, 5, 6, "Manny");
-    book.AddOrder(-115.2, 5, 7, "Edward");
-    book.AddOrder(-13.0, 5, 9, "Arthur");
-    book.RemoveOrder(93.0, 4);
-    book.UpdateOrder(13.0, 10, 6);
-    book.PrintOrders();
-
-    book.Matching();
-    return 0;
-}
